c54a49b35c378e6b729ea1b75d6d928bebc98f53
Julien Tanay <julien.tanay@gmail.com>
2021-01-05 20:25:09 +0100

    add test client

---
+++ src/client.js
@@ -0,0 +1,17 @@
+const { io } = require("socket.io-client");
+
+const socket = io("ws://localhost:3000/", {});
+
+socket.on("connect", () => {
+  console.log(`connect ${socket.id}`);
+});
+
+socket.on("disconnect", () => {
+  console.log(`disconnect`);
+});
+
+socket.on("output", console.log);
+
+setInterval(() => {
+  socket.emit("input", "ni");
+}, 2000);
