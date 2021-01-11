f8c91b708590412dd3874f7915b57591116db674
Julien Tanay <julien.tanay@gmail.com>
2021-01-11 13:10:25 +0100

    Use pkg to bundle server

---
+++ README.md
@@ -0,0 +1,24 @@
+# Pinyin Server
+
+NodeJS Pinyin Server
+
+## Usage
+
+Make sure you have NodeJS 12+ installed.
+
+Open a terminal and type:
+
+```
+npm i
+npm start
+```
+
+## Bundle executable
+
+Open a terminal and type:
+
+```
+npm run build
+```
+
+Then copy the files in `out/`
--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
+console.log("Starting server...");
+
 const io = require("socket.io")(3000);
 const getCandidates = require("./pinyin/ime_engine.js");
 
+console.log("Server listening on :3000");
+
 io.on("connection", (client) => {
   client.on("input", (data) => {
+    console.log(`Received ${data}`);
     client.emit("output", getCandidates(data));
   });
 });
